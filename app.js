2925c709cef9ef6f6117c5338d39dddedcc20a81
Chris <cldarling@gmail.com>
2025-03-25 21:35:23 -0400

    totally lost and don't know where to start

---
+++ app.js
@@ -0,0 +1,6 @@
+// petfinder api - 9IlicRCCU9TLIzmLGeHxHm47WMhhRAzyS2h3ImFFaQoTWiTmHF
+// petfinder secret - qlpng3u5H0rOZ6jTbeon4iHkODowPZkCZaUSE93n
+// curl -d "grant_type=client_credentials&client_id={CLIENT-ID}&client_secret={CLIENT-SECRET}" https://api.petfinder.com/v2/oauth2/token
+// request structure - curl -H "Authorization: Bearer {YOUR_ACCESS_TOKEN}" GET https://api.petfinder.com/v2/{CATEGORY}/{ACTION}?{parameter_1}={value_1}&{parameter_2}={value_2}
+// get a single animal type - GET https://api.petfinder.com/v2/types/{type}
+
